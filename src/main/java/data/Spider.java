943e6a93e975bf50dd4296a3bc3ef99d531945c4
secret1123 <19072390@qq.com>
2017-05-10 14:17:48 +0800

    update

--- src/main/java/data/Spider.java
+++ src/main/java/data/Spider.java
@@ -1,5 +1,6 @@
 package data;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -14,34 +15,59 @@ import java.io.IOException;
  * 2017/5/9 14:24.
  * RealEstate
  */
-public class Spider {
+public class Spider implements Runnable {
 
-    private static final String URL="http://bj.lianjia.com/ershoufang/dongcheng/pg";
-    private static int counter;
+    private static final String INIT_URL = "http://tj.lianjia.com/ershoufang/rs/";
+    private static final String URL = "http://tj.lianjia.com";
+    private Element element;
+
+    public Spider(Element element) {
+        this.element = element;
+    }
 
     public static void main(String[] args) throws IOException {
-        Document document = Jsoup.connect(URL).cookie("lianjia_uuid","c51d30c6-4dbf-4d52-b1de-de01f6493f72").get();
-        int total = Integer.parseInt(document.select("h2[class=total fl]").first().child(0).text());
-        int pages = (int)Math.ceil(total/30d);
-        for (int i = 0; i < pages; i++) {
-            System.out.println("page:" + (i+1));
-            page(i+1);
+        Document document = Jsoup.connect(INIT_URL).cookie("lianjia_uuid", "c51d30c6-4dbf-4d52-b1de-de01f6493f72").get();
+        Elements elements = document.select("div[data-role=ershoufang]").first().select("a[href^=/ershoufang]");
+        for (Element element : elements) {
+            Thread thread = new Thread(new Spider(element));
+            thread.start();
         }
     }
 
-    private static void page(int page){
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("test",true))){
-            Document document = Jsoup.connect(URL+page).cookie("lianjia_uuid","c51d30c6-4dbf-4d52-b1de-de01f6493f72").get();
+    @Override
+    public void run() {
+        String areaUrl = element.attr("href");
+        try {
+            Document areaDocument = Jsoup.connect(URL.concat(areaUrl)).cookie("lianjia_uuid", "c51d30c6-4dbf-4d52-b1de-de01f6493f72").get();
+            int total = Integer.parseInt(areaDocument.select("h2[class=total fl]").first().child(0).text());
+            int pages = (int) Math.ceil(total / 30d);
+            for (int i = 0; i < pages; i++) {
+                page(areaUrl, i + 1);
+                System.out.println(areaUrl.replace("/", "").replace("ershoufang", "") + ",page:" + (i + 1));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void page(String areaUrl, int page) {
+        String areaName = areaUrl.replace("/", "").replace("ershoufang", "");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data/tj/" + areaName, true))) {
+            Document document = Jsoup.connect(URL.concat(areaUrl) + "pg" + page).cookie("lianjia_uuid", "c51d30c6-4dbf-4d52-b1de-de01f6493f72").get();
             Elements elements = document.select("li[class=clear]");
             for (Element element : elements) {
+                String imageUrl = element.childNode(0).attr("href");
+                String id = imageUrl.substring(imageUrl.lastIndexOf("/") + 1, imageUrl.lastIndexOf("."));
                 String region = element.select("a[data-el=region]").first().text();
                 String totalPrice = element.select("div[class=totalPrice]").first().child(0).text();
                 Element houseInfoElement = element.select("div[class=houseInfo]").first();
                 String houseInfo = houseInfoElement.childNode(2).toString();
                 String unitPrice = element.select("div[class=unitPrice]").first().attr("data-price");
-                writer.write(region+"@"+houseInfo+"@"+totalPrice+"@"+unitPrice+"\n");
-                System.out.println("\t"+"counter:"+ ++counter);
+                writer.write(id + "@" + region + "@" + houseInfo + "@" + totalPrice + "@" + unitPrice + "\n");
             }
+        } catch (HttpStatusException e) {
+            System.out.println("http status code:" + e.getStatusCode());
+            page(areaUrl, page);
         } catch (IOException e) {
             e.printStackTrace();
         }
